c3ab6b289ea34bff67adb3232789bd4397f9f860
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-23 16:40:29 -0500

    Fixed spacing issues

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,15 +1,15 @@
 function bottleSong(bottles) {	
   let tmpBottles = Number(bottles);	
   while (tmpBottles > 2){	
-    console.log(`${tmpBottles} bottles of beer on the wall, ${tmpBottles}	bottles of beer.\nTake one down and pass it around, ${tmpBottles - 1} bottles of beer on the wall.`);	
+    console.log(`${tmpBottles} bottles of beer on the wall, ${tmpBottles} bottles of beer.\nTake one down and pass it around, ${tmpBottles - 1} bottles of beer on the wall.`);	
     tmpBottles --;	
   }	
   if (tmpBottles === 2){	
-    console.log(`${tmpBottles} bottles of beer on the wall, ${tmpBottles}	bottles of beer.\nTake one down and pass it around, ${tmpBottles - 1} bottle of beer on the wall.`);	
+    console.log(`${tmpBottles} bottles of beer on the wall, ${tmpBottles} bottles of beer.\nTake one down and pass it around, ${tmpBottles - 1} bottle of beer on the wall.`);	
     tmpBottles --;	
   }	
   if (tmpBottles === 1){	
-  console.log(`1 bottle of beer on the wall, 1 bottle of beer.\nTake one down	and pass it around, no more bottles of beer on the wall.\nGo to the store and	buy some more, ${bottles} bottles of beer on the wall.`);	
+  console.log(`1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nGo to the store and buy some more, ${bottles} bottles of beer on the wall.`);	
   }	
 };
 
