fec02e7f555302229ac4190363e7e8151067a96b
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-23 16:23:41 -0500

    First version of bottleSong

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,6 +1,16 @@
-function bottleSong() {
-  // Write your code here!
-  // test change
+function bottleSong(bottles) {	
+  let tmpBottles = bottles;	
+  while (tmpBottles > 2){	
+    console.log(`${tmpBottles} bottles of beer on the wall, ${tmpBottles}	 bottles of beer.\nTake one down and pass it around, ${tmpBottles - 1} bottles of beer on the wall.`);	
+    tmpBottles --;	
+  }	
+  if (tmpBottles === 2){	
+    console.log(`${tmpBottles} bottles of beer on the wall, ${tmpBottles}	 bottles of beer.\nTake one down and pass it around, ${tmpBottles - 1} bottle of beer on the wall.`);	
+    tmpBottles --;	
+  }	
+  if (tmpBottles === 1){	
+  console.log(`1 bottle of beer on the wall, 1 bottle of beer.\nTake one down	 and pass it around, no more bottles of beer on the wall.\nGo to the store and	 buy some more, ${bottles} bottles of beer on the wall.`);	
+  }	
 };
 
 bottleSong();
