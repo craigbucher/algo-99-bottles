d51b09fe1591c8e8a391baec986d2dce0e749f09
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-23 21:17:00 -0500

    initial python code

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,12 @@
-def bottle_song():
+def bottle_song(bottles):
 	# write your code here!
+    tmpBottles = bottles
+    while tmpBottles > 2:
+      print('%s bottles of beer on the wall. %s bottles of beer. Take one down, pass it around, %s bottles of beer on the wall.' % (tmpBottles, tmpBottles, tmpBottles-1))
+      tmpBottles -=1
+    print('%s bottles of beer on the wall. %s bottles of beer. Take one down, pass it around, %s bottle of beer on the wall.' % (tmpBottles, tmpBottles, tmpBottles-1))
+    tmpBottles -=1
+    print('1 bottle of beer on the wall. 1 bottle of beer. Take one down, pass it around, no more bottles of beer on the wall')
+    print('No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, %s bottles of beer on the wall.' % bottles)
 
-
-bottle_song()
+bottle_song(99)
